edfdac56fd540fff4fda0f58b001760903c5e101
LeopoldBriand <lebriand@boccard.fr>
2023-03-02 12:31:54 +0000

    feat: add weighted graphs

--- src/types.rs
+++ src/types.rs
@@ -8,5 +8,8 @@ pub struct Undirected;
 /// Directed graph implementation
 pub mod directed_graph;
 
+/// Weighted graph implementation
+pub mod weighted_graph;
+
 /// Undirected graph implementation
 pub mod undirected_graph;
